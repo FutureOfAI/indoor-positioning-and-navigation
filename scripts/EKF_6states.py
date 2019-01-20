21f76929e263e0ba7ce9c1a6599b70ee5dc3cba2
leo <742976921@qq.com>
2019-01-20 23:50:11 +0800

    Update EKF_6states.py

--- scripts/EKF_6states.py
+++ scripts/EKF_6states.py
@@ -14,12 +14,16 @@ import operator
 import socket
 import os
 import numpy as np
+from pyquaternion import Quaternion
 
 class EKF_6states(object):
     """docstring for EKF_6states"""
-    def __init__(self, wx, wy, wz, ax, ay, az, mx, my, mz, bgx, bgy, bgz, timestemp):
+    def __init__(self, wxp, wyp, wzp, wx, wy, wz, ax, ay, az, mx, my, mz, bgx, bgy, bgz, timestemp):
         super(EKF_6states, self).__init__()
         # save IMU variable
+        self._wxp = wxp
+        self._wyp = wyp
+        self._wzp = wzp
         self._wx = wx
         self._wy = wy
         self._wz = wz
@@ -34,6 +38,9 @@ class EKF_6states(object):
         self._bgz_h = bgz
         self._dt = timestemp
 
+    def initial_states(self):
+        pass
+
     def Predict(self):
         pass
 
@@ -45,4 +52,24 @@ class EKF_6states(object):
 
     # get direction cosine matrix from gyroscope meter
     def DCM_calculate(self):
-        return self._wx
+        self._wx = self._wx - self._bgx_h
+        self._wy = self._wy - self._bgy_h
+        self._wz = self._wz - self._bgz_h
+
+        self._d1 = self._wx * self._dt / 2
+        self._d2 = self._wy * self._dt / 2
+        self._d3 = self._wz * self._dt / 2
+
+        self._d1p = self._wxp * self._dt / 2
+        self._d2p = self._wyp * self._dt / 2
+        self._d3p = self._wzp * self._dt / 2
+
+        self._d0_s = np.square(self._d1) + np.square(self._d2) + np.square(self._d3)
+
+        self._q1 = 1 - np.square(self._d0_s)
+        self._q2 = self._d1 - (self._d0_s*self._d1 + self._d3p*self._d2 + self._d2p*self._d3)/6
+        self._q3 = self._d2 - (self._d0_s*self._d2 + self._d1p*self._d3 + self._d3p*self._d1)/6
+        self._q4 = self._d3 - (self._d0_s*self._d3 + self._d2p*self._d1 + self._d1p*self._d2)/6
+
+        self._delta_Q = Quaternion(self._q1, -self._q2, self._q3, self._q4)
+
