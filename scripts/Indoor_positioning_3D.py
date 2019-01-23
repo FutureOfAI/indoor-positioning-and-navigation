9147cc6453a433f000d77bebc88339782b24446a
leo <742976921@qq.com>
2019-01-23 22:09:07 +0800

    Update Indoor_positioning_3D.py

--- scripts/Indoor_positioning_3D.py
+++ scripts/Indoor_positioning_3D.py
@@ -21,7 +21,28 @@ from Queue import Queue
 # acc = np.zeros([1,3])
 # gro = np.zeros([1,3])
 # mag = np.zeros([1,3])
-print "test2"
+
+# Initialize EKF 6-states parameters
+#ekf6 = EKF6.EKF_6states(0.01)
+
+# IMU Initialization
+SETTINGS_FILE = "RTIMULib"
+s = RTIMU.Settings(SETTINGS_FILE)
+imu = RTIMU.RTIMU(s)
+if (not imu.IMUInit()):
+	print ("IMU Initialize Failed.")
+imu.setGyroEnable(True)
+imu.setAccelEnable(True)
+imu.setCompassEnable(True)
+
+if imu.IMURead():
+	data = imu.getIMUData()
+	fusionPose = data["fusionPose"]
+	# acc = data["accel"]
+	# gro = data["gyro"]
+	# mag = data["compass"]
+	print fusionPose
+
 # IMU Thread
 class Get_IMU_Data(threading.Thread):
 	def __init__(self, t_name, queue):
@@ -60,28 +81,6 @@ class EKF_Cal_Euler(threading.Thread):
 
 # main Thread
 def main():
-	# Initialize EKF 6-states parameters
-	ekf6 = EKF6.EKF_6states(0.01)
-
-	# IMU Initialization
-	SETTINGS_FILE = "RTIMULib"
-	s = RTIMU.Settings(SETTINGS_FILE)
-	imu = RTIMU.RTIMU(s)
-	if (not imu.IMUInit()):
-		print ("IMU Initialize Failed.")
-	imu.setGyroEnable(True)
-	imu.setAccelEnable(True)
-	imu.setCompassEnable(True)
-
-	if imu.IMURead():
-		data = imu.getIMUData()
-		fusionPose = data["fusionPose"]
-		# acc = data["accel"]
-		# gro = data["gyro"]
-		# mag = data["compass"]
-		print fusionPose
-
-	print "test1"
 
 	queue = Queue()
 	imu = Get_IMU_Data('IMU.', queue)
