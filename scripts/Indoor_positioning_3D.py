787c24d6b15c23d39aef7b8dd0c3936a3f7bde19
leo <742976921@qq.com>
2019-01-23 19:38:35 +0800

    Update Indoor_positioning_3D.py

--- scripts/Indoor_positioning_3D.py
+++ scripts/Indoor_positioning_3D.py
@@ -40,10 +40,12 @@ def Get_IMU_Data():
 		acc = data["accel"]
 		gro = data["gyro"]
 		mag = data["compass"]
+		print ("task IMU")
 	time.sleep(10)
 
 IMU_thread = threading.Thread(target = Get_IMU_Data)
 IMU_thread.start()
 
 while True:
+	print ("loop")
 	time.sleep(10)
