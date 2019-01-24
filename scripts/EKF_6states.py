ffe278d8afbdd4a1d5abca573775cf002e7f9645
leo <742976921@qq.com>
2019-01-24 19:56:22 +0800

    ekf6

--- scripts/EKF_6states.py
+++ scripts/EKF_6states.py
@@ -30,9 +30,7 @@ class EKF_6states(object):
     def Predict(self, wxp, wyp, wzp, wx, wy, wz, bgx_h, bgy_h, bgz_h, QE_B_m, s6_xz_h, s6_P00_z, s6_Q_z):
         s6_F_z = np.zeros([6,6])
 
-        #DC_E_B_m, QE_B_m = self.DCM_calculate(wxp, wyp, wzp, wx, wy, wz, bgx_h, bgy_h, bgz_h, QE_B_m)
-
-        DC_E_B_m = np.ones([3,3])
+        DC_E_B_m, QE_B_m = self.DCM_calculate(wxp, wyp, wzp, wx, wy, wz, bgx_h, bgy_h, bgz_h, QE_B_m)
 
         s6_F_z[0,3] = -DC_E_B_m[0,0]
         s6_F_z[0,4] = -DC_E_B_m[1,0]
@@ -46,11 +44,11 @@ class EKF_6states(object):
         s6_F_z[2,3] = -DC_E_B_m[1,2]
         s6_F_z[2,3] = -DC_E_B_m[2,2]
 
-        #s6_phi_z = LA.expm(s6_F_z*self._dt)
-        #s6_xz_h = s6_phi_z.dot(s6_xz_h)
-        #s6_P00_z = s6_phi_z.dot(s6_P00_z).dot(s6_phi_z.T) + s6_Q_z*self._dt
+        s6_phi_z = LA.expm(s6_F_z*self._dt)
+        s6_xz_h = s6_phi_z.dot(s6_xz_h)
+        s6_P00_z = s6_phi_z.dot(s6_P00_z).dot(s6_phi_z.T) + s6_Q_z*self._dt
 
-        return s6_P00_z, QE_B_m
+        return s6_P00_z
 
     def Update(self, ax, ay, az, mx, my, mz, s6_P00_z, s6_H, s6_R):
         C_E_B_e = self.TRIAD(ax, ay, az, mx, my, mz)
