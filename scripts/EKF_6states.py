fcaecdf9e3ea5df49e9221b2155598a79bed5bb5
leo <742976921@qq.com>
2019-01-24 16:22:46 +0800

    measurement

--- scripts/EKF_6states.py
+++ scripts/EKF_6states.py
@@ -69,8 +69,7 @@ class EKF_6states(object):
 
         return s6_P00_z, s6_z_update
 
-    def Measurement(self, dtheda_xh, dtheda_yh, dtheda_zh, bgx_h, bgy_h, bgz_h, s6_z_update, w_EB_B_xm, w_EB_B_ym, w_EB_B_zm):
-        global w_EB_B_xm, w_EB_B_ym, w_EB_B_zm
+    def Measurement(self, dtheda_xh, dtheda_yh, dtheda_zh, bgx_h, bgy_h, bgz_h, s6_z_update):
 
         dtheda_xh = dtheda_xh + s6_z_update[1]
         dtheda_yh = dtheda_yh + s6_z_update[2]
@@ -80,11 +79,7 @@ class EKF_6states(object):
         bgy_h = bgy_h + s6_z_update[5]
         bgz_h = bgz_h + s6_z_update[6]
 
-        w_EB_B_xm = w_EB_B_xm - bgx_h
-        w_EB_B_ym = w_EB_B_ym - bgy_h
-        w_EB_B_zm = w_EB_B_zm - bgz_h
-
-        return dtheda_xh, dtheda_yh, dtheda_zh, bgx_h, bgy_h, bgz_h, w_EB_B_xm, w_EB_B_ym, w_EB_B_zm
+        return dtheda_xh, dtheda_yh, dtheda_zh, bgx_h, bgy_h, bgz_h
 
     # get direction cosine matrix from gyroscopemeter
     def DCM_calculate(self, wxp, wyp, wzp, wx, wy, wz, bgx_h, bgy_h, bgz_h, QE_B_m):
