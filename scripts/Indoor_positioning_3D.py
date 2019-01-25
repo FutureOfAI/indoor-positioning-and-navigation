06a3a4e2b65a14ac5c75a03b8abef7e0ad233f98
leo <742976921@qq.com>
2019-01-25 16:27:58 +0800

    Update Indoor_positioning_3D.py

--- scripts/Indoor_positioning_3D.py
+++ scripts/Indoor_positioning_3D.py
@@ -144,7 +144,7 @@ class Get_IMU_Data(threading.Thread):
 				acc = data["accel"]
 				gro = data["gyro"]
 				mag = data["compass"]
-			time.sleep(poll_interval*2/1000)
+			time.sleep(0.01)
 
 # DWM Thread
 class Get_UWB_Data(threading.Thread):
@@ -179,11 +179,11 @@ class EKF_Cal_Euler(threading.Thread):
 			dQ2 = Quaternion(q1, q2, q3, q4)
 			QE_B_m = dQ2.normalised * QE_B_m.normalised
 			Angle = ekf6.quatern2euler(QE_B_m)
-			#print (Angle*r2d)
-			end_time = time.time()
-			print (psutil.cpu_percent())
+			print (Angle*r2d)
+			#end_time = time.time()
+			#print (psutil.cpu_percent())
 			#print (end_time-start_time)
-			#time.sleep(0.5)
+			time.sleep(0.01)
 
 # main Thread
 def main():
