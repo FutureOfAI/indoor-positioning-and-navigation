4aa7fdc0604f8d783bf9089d9661a195c7f76ab0
leo <742976921@qq.com>
2019-01-25 13:01:58 +0800

    Update Indoor_positioning_3D.py

--- scripts/Indoor_positioning_3D.py
+++ scripts/Indoor_positioning_3D.py
@@ -146,6 +146,7 @@ class Get_IMU_Data(threading.Thread):
 				acc = data["accel"]
 				gro = data["gyro"]
 				mag = data["compass"]
+				print (acc)
 			else:
 				print ("IMU Failed!")
 			time.sleep(0.01)
@@ -182,7 +183,7 @@ class EKF_Cal_Euler(threading.Thread):
 			dQ2 = Quaternion(q1, q2, q3, q4)
 			QE_B_m = dQ2.normalised * QE_B_m.normalised
 			Angle = ekf6.quatern2euler(QE_B_m)
-			print (Angle*r2d)
+			#print (Angle*r2d)
 			time.sleep(0.01)
 
 # main Thread
