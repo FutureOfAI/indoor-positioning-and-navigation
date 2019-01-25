61f77f2f8c3b1788f60e105610f0c4c43982b147
leo <742976921@qq.com>
2019-01-25 16:24:31 +0800

    Update Indoor_positioning_3D_pool.py

--- scripts/Indoor_positioning_3D_pool.py
+++ scripts/Indoor_positioning_3D_pool.py
@@ -182,9 +182,9 @@ class EKF_Cal_Euler(threading.Thread):
 			dQ2 = Quaternion(q1, q2, q3, q4)
 			QE_B_m = dQ2.normalised * QE_B_m.normalised
 			Angle = ekf6.quatern2euler(QE_B_m)
-			#print (Angle*r2d)
+			print (Angle*r2d)
 			end_time = time.time()
-			print (psutil.cpu_percent())
+			#print (psutil.cpu_percent())
 			#print (end_time-start_time)
 			#time.sleep(0.5)
 
