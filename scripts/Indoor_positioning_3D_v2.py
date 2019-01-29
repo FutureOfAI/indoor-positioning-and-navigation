409369a3876e199d6d050ca76898f3e673641561
leo <742976921@qq.com>
2019-01-29 22:30:44 +0800

    Update Indoor_positioning_3D_v2.py

--- scripts/Indoor_positioning_3D_v2.py
+++ scripts/Indoor_positioning_3D_v2.py
@@ -17,6 +17,10 @@ from pyquaternion import Quaternion
 from numpy import linalg as LA
 import EKF_6states as EKF6
 from Queue import Queue
+import DW1000
+import monotonic
+import DW1000Constants as C
+import RPi.GPIO as GPIO 
 import psutil
 
 # initial database matrix
@@ -39,6 +43,46 @@ imu.setCompassEnable(True)
 poll_interval = imu.IMUGetPollInterval()
 
 # DWM Initialize
+n_23=0
+n_24=0
+n_25=0
+n_26=0
+n_27=0
+n_29=0
+
+
+Position_Flag=0
+DistanceFinish_Flag=0
+Same_tag_flag=0
+lastActivity = 0
+expectedMsgId = C.POLL
+protocolFailed = False
+sentAck = False
+receivedAck = False
+LEN_DATA =25
+data = [0] * LEN_DATA
+LEN_TAG=6
+tag=[0]*LEN_TAG
+timePollAckSentTS = 0
+timePollAckReceivedTS = 0
+timePollReceivedTS = 0
+timeRangeReceivedTS = 0
+timePollSentTS = 0
+timeRangeSentTS = 0
+timeComputedRangeTS = 0
+REPLY_DELAY_TIME_US = 7000 
+
+PIN_RST = 17
+PIN_IRQ = 19
+PIN_SS = 27
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(PIN_RST,GPIO.IN)
+DW1000.begin(PIN_IRQ)
+DW1000.setup(PIN_SS)
+print("DW1000 initialized")
+print("############### ANCHOR ##############")
+DW1000.generalConfiguration("82:17:5B:D5:A9:9A:E2:9B", C.MODE_LONGDATA_RANGE_ACCURACY)
+DW1000.setAntennaDelay(C.ANTENNA_DELAY_RASPI)
 
 # set DWM1000 mutex for muti-anchors distance measure
 UWB_Mutex = threading.Lock()
@@ -146,6 +190,173 @@ s6_R[0,0] = R_factor*np.square(0.5*d2r)
 s6_R[1,1] = R_factor*np.square(0.5*d2r)
 s6_R[2,2] = R_factor*np.square(2.5*d2r)
 
+
+def millis():
+	"""
+	This function returns the value (in milliseconds) of a clock which never goes backwards. It detects the inactivity of the chip and
+	is used to avoid having the chip stuck in an undesirable state.
+	"""    
+	return int(round(monotonic.monotonic() * C.MILLISECONDS))
+
+def handleSent():
+	"""
+	This is a callback called from the module's interrupt handler when a transmission was successful. 
+	It sets the sentAck variable as True so the loop can continue.
+	"""            
+	global sentAck
+	sentAck = True
+
+def handleReceived():
+	"""
+	This is a callback called from the module's interrupt handler when a reception was successful. 
+	It sets the received receivedAck as True so the loop can continue.
+	"""
+	global receivedAck
+	receivedAck = True
+
+DW1000.registerCallback("handleSent", handleSent)
+DW1000.registerCallback("handleReceived", handleReceived)
+
+
+def noteActivity():
+	"""
+	This function records the time of the last activity so we can know if the device is inactive or not.
+	"""        
+	global lastActivity
+	lastActivity = millis()
+
+def Anchor_resetInactive():
+	"""
+	This function restarts the default polling operation when the device is deemed inactive.
+	"""    
+	global expectedMsgId
+	DW1000.generalConfiguration("82:17:5B:D5:A9:9A:E2:9B", C.MODE_LONGDATA_RANGE_ACCURACY) 
+	DW1000.registerCallback("handleSent", handleSent)
+	DW1000.registerCallback("handleReceived", handleReceived)
+	DW1000.setAntennaDelay(C.ANTENNA_DELAY_RASPI)       
+	expectedMsgId = C.POLL
+	print("run")
+	receiver()
+	noteActivity()
+
+def transmitPollAck():
+	"""
+	This function sends the polling acknowledge message which is used to confirm the reception of the polling message. 
+	"""        
+	global data
+	DW1000.newTransmit()
+	data[0] = C.POLL_ACK
+	DW1000.setDelay(REPLY_DELAY_TIME_US, C.MICROSECONDS)
+	DW1000.setData(data, LEN_DATA)
+	DW1000.startTransmit()
+
+def transmitRangeAcknowledge():
+	"""
+	This functions sends the range acknowledge message which tells the tag that the ranging function was successful and another ranging transmission can begin.
+	"""
+	global data
+	DW1000.newTransmit()
+	data[0] = C.RANGE_REPORT
+
+	DW1000.setData(data, LEN_DATA)
+	DW1000.startTransmit()
+
+def transmitRangeFailed():
+	"""
+	This functions sends the range failed message which tells the tag that the ranging function has failed and to start another ranging transmission.
+	"""    
+	global data
+	DW1000.newTransmit()
+	data[0] = C.RANGE_FAILED
+	DW1000.setData(data, LEN_DATA)
+	DW1000.startTransmit()
+
+def receiver():
+	"""
+	This function configures the chip to prepare for a message reception.
+	"""
+	global data
+	DW1000.newReceive()
+	DW1000.receivePermanently()
+	DW1000.startReceive()
+
+receiver()
+noteActivity()
+
+def computeRangeAsymmetric():
+	"""
+	This is the function which calculates the timestamp used to determine the range between the devices.
+	"""
+	global timeComputedRangeTS
+	round1 = DW1000.wrapTimestamp(timePollAckReceivedTS - timePollSentTS)
+	reply1 = DW1000.wrapTimestamp(timePollAckSentTS - timePollReceivedTS)
+	round2 = DW1000.wrapTimestamp(timeRangeReceivedTS - timePollAckSentTS)
+	reply2 = DW1000.wrapTimestamp(timeRangeSentTS - timePollAckReceivedTS)
+	timeComputedRangeTS = (round1 * round2 - reply1 * reply2) / (round1 + round2 + reply1 + reply2)
+
+def loop():
+	global uwb_array,gyro_array,gyro_count,sentAck,n_ekf_start,start,n_23,n_24,n_25,n_26,n_27,n_29,receivedAck, timePollAckSentTS, timePollReceivedTS, timePollSentTS, timePollAckReceivedTS, timeRangeReceivedTS, protocolFailed, data, expectedMsgId,expectedMsgID, timeRangeSentTS,Same_tag_flag,DistanceFinish_Flag,Position_Flag,EKF_start,EKF_message,EKF_New,EKF_Update
+
+	if Position_Flag==0:
+		if sentAck == False and receivedAck == False:
+			if ((millis() - lastActivity) > C.RESET_PERIOD):
+				Anchor_resetInactive()
+				return
+		if sentAck:
+			sentAck = False
+			msgId = data[0]
+			if Same_tag_flag == data[16]:
+				if msgId == C.POLL_ACK:
+					timePollAckSentTS = DW1000.getTransmitTimestamp()
+					noteActivity()
+		if receivedAck:
+			receivedAck = False
+			data = DW1000.getData(LEN_DATA)
+			msgId = data[0]
+			if msgId == C.POLL:
+				DistanceFinish_Flag =1
+				Same_tag_flag = data[16]
+				protocolFailed = False
+				timePollReceivedTS = DW1000.getReceiveTimestamp()
+				expectedMsgId = C.RANGE
+				transmitPollAck()
+				noteActivity()
+			elif msgId == C.RANGE :
+				if (DistanceFinish_Flag == 1 and Same_tag_flag == data[16]):
+					DistanceFinish_Flag = 0
+					timeRangeReceivedTS = DW1000.getReceiveTimestamp()
+					expectedMsgId = C.POLL
+					if protocolFailed == False:
+						timePollSentTS = DW1000.getTimeStamp(data, 1)
+						timePollAckReceivedTS = DW1000.getTimeStamp(data, 6)
+						timeRangeSentTS = DW1000.getTimeStamp(data, 11)
+						computeRangeAsymmetric()
+						transmitRangeAcknowledge()
+						distance = (timeComputedRangeTS % C.TIME_OVERFLOW) * C.DISTANCE_OF_RADIO
+
+						if data[16]==23:
+							print("Tag: %.2d"%(data[16]))
+							print("Distance1: %.2f m" %(distance))
+						if data[16]==25:
+							print("Tag: %.2d"%(data[16]))
+							print("Distance2: %.2f m" %(distance))
+						if data[16]==26:
+							print("Tag: %.2d"%(data[16]))
+							print("Distance3: %.2f m" %(distance))
+						if data[16]==27:
+							print("Tag: %.2d"%(data[16]))
+							print("Distance4: %.2f m" %(distance))
+						if data[16]==24:
+							print("Tag: %.2d"%(data[16]))
+							print("Distance4: %.2f m" %(distance))
+						if data[16]==29:
+							print("Tag: %.2d"%(data[16]))
+							print("Distance4: %.2f m" %(distance))
+					else:
+						transmitRangeFailed()
+					noteActivity()
+
+
 # Initialize EKF 6-states parameters 0.01s
 ekf6 = EKF6.EKF_6states(0.01)
 
@@ -177,6 +388,7 @@ class Get_UWB_Data(threading.Thread):
 		self.data = queue
 	def run(self):
 		while True:
+			loop()
 			time.sleep(0.01)
 
 # 6-states EKF thread
@@ -230,17 +442,17 @@ class Save_Data(threading.Thread):
 # main Thread
 def main():
 	queue = Queue()
-	imu_queue = Get_IMU_Data('IMU.', queue)
+	# imu_queue = Get_IMU_Data('IMU.', queue)
 	uwb_queue = Get_UWB_Data('UWB.', queue)
-	euler_queue = EKF_Cal_Euler('Euler.',queue)
+	# euler_queue = EKF_Cal_Euler('Euler.',queue)
 	# data_queue = Save_Data('Save Data', queue)
-	imu_queue.start()
+	# imu_queue.start()
 	uwb_queue.start()
-	euler_queue.start()
+	# euler_queue.start()
 	# data_queue.start()
-	imu_queue.join()
+	# imu_queue.join()
 	uwb_queue.join()
-	euler_queue.join()
+	# euler_queue.join()
 	# data_queue.join()
 	print ('All threads terminate!')
 
