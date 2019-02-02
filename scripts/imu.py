1eb309baafdd910f57bf0ec1833c51951771e191
leo <742976921@qq.com>
2019-02-02 17:26:25 +0800

    Update imu.py

--- scripts/imu.py
+++ scripts/imu.py
@@ -216,6 +216,8 @@ while True:
             lcd.clear()
             lcd.message(imu_sentence)
             if Eluer_buf_cnt<100:
+              if yaw>180:
+                yaw = yaw-360
               Eluer_buf[Eluer_buf_cnt,:] = np.array([yaw, heading, roll, pitch])
             Eluer_buf_cnt = Eluer_buf_cnt + 1
 
