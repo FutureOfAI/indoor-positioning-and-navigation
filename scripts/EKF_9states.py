62ac84d9c01c9cdb51e22a80227f0c3848bde439
leo <742976921@qq.com>
2019-02-05 23:00:14 +0800

    Create EKF_9states.py

---
+++ scripts/EKF_9states.py
@@ -0,0 +1,20 @@
+"""
+This python module contains 9-states EKF's pridict, update and measurement function
+output is Positioning
+"""
+import numpy as np
+
+class EKF_9states(object):
+	"""docstring for ClassName"""
+	def __init__(self, timestemp):
+		super(EKF_9states, self).__init__()
+		self.dt = timestemp
+
+	def Predict():
+		pass
+
+	def Update():
+		pass
+
+	def Measurement():
+		pass
