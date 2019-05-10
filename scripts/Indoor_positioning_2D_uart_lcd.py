553c265ca3c12f85699c15ed81d461bac1ea056c
leo <arleyyao@gmail.com>
2019-05-10 21:40:18 +0800

    Update Indoor_positioning_2D_uart_lcd.py

--- scripts/Indoor_positioning_2D_uart_lcd.py
+++ scripts/Indoor_positioning_2D_uart_lcd.py
@@ -1,4 +1,7 @@
+import time
+import serial
 import Adafruit_CharLCD as LCD
+import binascii
 
 # Raspberry Pi pin configuration:
 lcd_rs        = 6  # Note this might need to be changed to 21 for older revision Pi's.
@@ -16,7 +19,28 @@ lcd_rows    = 2
 lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
                            lcd_columns, lcd_rows)
 
-lcd.message("test!")
+# lcd.message("test!")
 
-while True:
-	time.sleep(1000.0)
+distance=[0]*10000
+ser = serial.Serial("/dev/ttyAMA0", 57600,bytesize = 8,parity = 'N',stopbits = 1)
+if ser.isOpen == False:
+    ser.open()
+n=0
+A10=0
+A11=0
+A12=0
+A13=0
+sum=0
+
+try:
+	while True:
+		ser.write("DistanceOutON"+"\r"+"\n")
+
+		size = ser.inWaiting()
+		if size != 0:
+			reponse = ser.read(size)
+			print binascii.hexlify(reponse)
+		time.sleep(0.1)
+
+except KeyboardInterrupt:
+	ser.close()
