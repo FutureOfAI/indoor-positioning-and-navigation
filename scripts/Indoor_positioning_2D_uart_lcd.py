754074ce9fe0fd75a6801c7ba7ed6075ee49de65
leo <arleyyao@gmail.com>
2019-05-10 21:30:10 +0800

    Create Indoor_positioning_2D_uart_lcd.py

---
+++ scripts/Indoor_positioning_2D_uart_lcd.py
@@ -0,0 +1,22 @@
+import Adafruit_CharLCD as LCD
+
+# Raspberry Pi pin configuration:
+lcd_rs        = 6  # Note this might need to be changed to 21 for older revision Pi's.
+lcd_en        = 22
+lcd_d4        = 25
+lcd_d5        = 24
+lcd_d6        = 23
+lcd_d7        = 18
+
+# Alternatively specify a 16x2 LCD.
+lcd_columns = 16
+lcd_rows    = 2
+
+# Initialize the LCD using the pins above.
+lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
+                           lcd_columns, lcd_rows)
+
+lcd.message("test!")
+
+while True:
+	time.sleep(1000.0)
