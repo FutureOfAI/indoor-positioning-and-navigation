d132e32d2e4374a14bea793347a9dd5eac8de204
leo <arleyyao@gmail.com>
2019-05-11 11:54:32 +0800

    Update imu.py

--- scripts/imu.py
+++ scripts/imu.py
@@ -21,8 +21,8 @@ lcd_d6        = 23
 lcd_d7        = 18
 
 # Alternatively specify a 16x2 LCD.
-lcd_columns = 16
-lcd_rows    = 2
+lcd_columns = 20
+lcd_rows    = 4
 
 # Initialize the LCD using the pins above.
 lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
